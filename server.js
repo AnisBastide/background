ad4a4b1223279dd21c02e207708da6d61af1d814
Marion Meurant <marion.meurant@edu.itescia.fr>
2023-10-06 15:24:13 +0200

    Add test to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const express = require('express')
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const app = express()
 const port = 3000
 var morgan = require('morgan')
@@ -47,12 +47,12 @@ app.get('/background', async (req, res) => {
     const backgrounds = await prisma.backgroundImage.findMany()
     const number = getRandomInt(backgrounds.length - 1)
     const background = backgrounds[number]
-    const treasures = await getTreasures(10,background.width,background.height)
+    const treasures = await getTreasures(10, background.width, background.height)
     const response = {
-        map:background.link,
-        width:background.width,
-        height:background.height,
-        treasures:treasures
+        map: background.link,
+        width: background.width,
+        height: background.height,
+        treasures: treasures
     }
     res.send(response)
 });
@@ -128,16 +128,18 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-async function getTreasures(treasuresCount,width,height) {
+async function getTreasures(treasuresCount, width, height) {
     treasures = []
     const treasuresInDb = await prisma.treasures.findMany()
     for (let i = 0; i < treasuresCount; i++) {
         const number = getRandomInt(treasuresInDb.length - 1)
         const treasure = treasuresInDb[number]
-        treasures.push({id:i,posX:getRandomInt(width),posY:getRandomInt(height),image:treasure.image,value:treasure.score})
+        treasures.push({ id: i, posX: getRandomInt(width), posY: getRandomInt(height), image: treasure.image, value: treasure.score })
     }
 
-   return treasures
+    return treasures
 }
 
-
+module.exports = {
+    getTreasures,
+};
---
+++ server.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const express = require('express');
+const server = require('./server.js');
+
+const app = express();
+
+describe('Test de la route /background', () => {
+    it('devrait renvoyer des informations valides', async () => {
+        const response = await request(app).get('/background');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('map');
+        expect(response.body).toHaveProperty('width');
+        expect(response.body).toHaveProperty('height');
+        expect(response.body).toHaveProperty('treasures');
+    });
+});
+
+describe('Test de getTreasures', () => {
+    it('devrait être ok', async () => {
+        const treasures = await server.getTreasures(2, 10, 10);
+
+        expect(Array.isArray(treasures)).toBe(true);
+        expect(treasures.length).toBe(10);
+    });
+});
